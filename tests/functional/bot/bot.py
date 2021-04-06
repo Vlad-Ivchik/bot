4b2bea4778c45677609118264c99ba9a85267bd3
Vlad <ivchik.vlad@gmail.com>
2021-04-06 23:28:40 +0300

    test find dotenv

--- tests/functional/bot/bot.py
+++ tests/functional/bot/bot.py
@@ -1,18 +1,23 @@
 import os
 import pytest
+from dotenv import load_dotenv, find_dotenv
 from starlette import status
 from starlette.testclient import TestClient
 from asgi import app
 
 client = TestClient(app)
 
+load_dotenv(find_dotenv())
+
+bot_token = os.environ.get("BOT_TOKEN")
+pythonpath = os.environ.get("PYTHONPATH")
 
 @pytest.mark.functional
 def test_bot():
     response = client.get("/config")
     assert response.status_code == status.HTTP_200_OK
     payload = response.json()
-    data = {"bot_token": os.getenv('BOT_TOKEN'), "pythonpath": os.getenv('PYTHONPATH')}
+    data = {"bot_token": bot_token, "pythonpath": pythonpath}
     assert payload == data
 
 
@@ -21,5 +26,5 @@ def test_bot_heroku():
     response = client.get("https://bot-vlad-z43.herokuapp.com/config/")
     assert response.status_code == status.HTTP_200_OK
     payload = response.json()
-    data = {"bot_token": os.getenv('BOT_TOKEN'), "pythonpath": os.getenv('PYTHONPATH')}
+    data = {"bot_token": bot_token, "pythonpath": pythonpath}
     assert payload == data
