a45ccda9adfc72120f5056561f57c3f02b60dadf
Vlad <ivchik.vlad@gmail.com>
2021-04-08 22:56:17 +0300

    bot message

--- src/bot/asgi.py
+++ src/bot/asgi.py
@@ -1,10 +1,6 @@
 import aiohttp
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from starlette import status
-from starlette.requests import Request
-from starlette.responses import Response
-
 from bot.config import settings
 from bot.schema import Update, MessageReply
 from bot.util import debug
@@ -23,14 +19,25 @@ async def handle_settings():
 @app.post("/webhook/")
 async def tg_webhook(update: Update):
     try:
-        reply = MessageReply(
+
+        #.................
+
+        await respond_to_td(
             chat_id=update.message.chat.id,
-            text=update.message.text.capitalize(),
+            text=update.message.text.capitalize()
         )
-        async with aiohttp.ClientSession() as sesss:
-            url = f"https://api.telegram.org/bot{settings.bot_token}/sendMessage"
-            async with  sesss.post(url, json=reply.dict()) as resp:
-                payload = await resp.json()
-                debug(resp, payload)
     finally:
         return {"ok": True}
+
+
+async def respond_to_td(chat_id: int, text: str):
+
+    reply = MessageReply(
+        chat_id=chat_id,
+        text=text,
+    )
+    async with aiohttp.ClientSession() as sesss:
+        url = f"https://api.telegram.org/bot{settings.bot_token}/sendMessage"
+        async with sesss.post(url, json=reply.dict()) as resp:
+            payload = await resp.json()
+            debug(resp, payload)
