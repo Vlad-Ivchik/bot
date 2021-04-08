e84d03419abb12dae776be515bbe60c7f1f38823
Vlad-Ivchik <76444856+Vlad-Ivchik@users.noreply.github.com>
2021-04-08 22:42:04 +0300

    Bot (#2)
    * test find dotenv
    
    * bot message

--- src/bot/asgi.py
+++ src/bot/asgi.py
@@ -1,7 +1,12 @@
+import aiohttp
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import Response
 
 from bot.config import settings
+from bot.schema import Update, MessageReply
 from bot.util import debug
 
 load_dotenv()
@@ -13,3 +18,19 @@ app = FastAPI()
 async def handle_settings():
     debug(settings)
     return settings
+
+
+@app.post("/webhook/")
+async def tg_webhook(update: Update):
+    try:
+        reply = MessageReply(
+            chat_id=update.message.chat.id,
+            text=update.message.text.capitalize(),
+        )
+        async with aiohttp.ClientSession() as sesss:
+            url = f"https://api.telegram.org/bot{settings.bot_token}/sendMessage"
+            async with  sesss.post(url, json=reply.dict()) as resp:
+                payload = await resp.json()
+                debug(resp, payload)
+    finally:
+        return {"ok": True}
